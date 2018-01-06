f32a6cef2cb95a24c5568db081683de0d347ca4c
David Castro <dc84@st-andrews.ac.uk>
2018-01-06 15:59:34 +0000

    A first attempt to rewriting rules

--- src/Language/Poly/C.hs
+++ src/Language/Poly/C.hs
@@ -1,48 +1,83 @@
 -- Wrapper on top of Language.Poly + C-like expressions
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeInType #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE ViewPatterns #-}
 module Language.Poly.C
 ( PrimTy (..)
+, TyPrim (..)
 , C
 , CInt
 , CFloat
 , CNum
 , CType
 , CExpr (..)
-, CCore (..)
+, CCore
+, ECore (..)
 , module X
 ) where
+
+import Data.Singletons
+  ( SingI (..)
+  , SingKind(..)
+  , SomeSing(..) )
+import Data.Singletons.TypeLits as Sing
 
 import Language.Poly as X
 
 -- Primitive functions and operations
-data PrimTy = Int32 | Float32 | Vector Int PrimTy
+data TyPrim = TInt32 | TFloat32 | TVector Sing.Nat TyPrim
+data PrimTy = Int32  | Float32  | Vector Integer PrimTy
 
-type family C (a :: PrimTy) :: Type PrimTy where
+data instance Sing (t :: TyPrim) where
+    SInt32 :: Sing 'TInt32
+    SFloat32 :: Sing 'TFloat32
+    SVector :: Sing i -> Sing t -> Sing ('TVector i t)
+instance SingI 'TInt32 where
+  sing = SInt32
+instance SingI 'TFloat32 where
+  sing = SFloat32
+instance (SingI i, SingI t) => SingI ('TVector i t) where
+  sing = SVector sing sing
+
+instance SingKind TyPrim where
+  type DemoteRep TyPrim = PrimTy
+  fromSing SInt32 = Int32
+  fromSing SFloat32 = Float32
+  fromSing (SVector i t) = Vector (fromSing i) (fromSing t)
+  toSing Int32 = SomeSing SInt32
+  toSing Float32 = SomeSing SFloat32
+  toSing (Vector (toSing -> SomeSing i) (toSing -> SomeSing t)) =
+      SomeSing $ SVector i t
+
+type family C (a :: TyPrim) :: Type TyPrim where
   C a = 'TPrim a
 
-type CInt   = C 'Int32
-type CFloat = C 'Float32
+type CInt   = C 'TInt32
+type CFloat = C 'TFloat32
 
-class CNum (a :: PrimTy) where
-instance CNum 'Int32 where
-instance CNum 'Float32 where
+class CNum (a :: TyPrim) where
+instance CNum 'TInt32 where
+instance CNum 'TFloat32 where
 
 type CType = Type PrimTy
 
-data CExpr (t :: Type PrimTy) where
+data CExpr (t :: Type TyPrim) where
     Plus  :: CNum a => CExpr (C a ':-> C a ':-> C a)
     Minus :: CNum a => CExpr (C a ':-> C a ':-> C a)
     Mult  :: CNum a => CExpr (C a ':-> C a ':-> C a)
     Div   :: CNum a => CExpr (C a ':-> C a ':-> C a)
     Mod   :: CNum a => CExpr (C a ':-> C a ':-> C a)
-    Get   :: CExpr (C ('Vector i a) ':-> CInt ':-> C a)
-    Put   :: CExpr (CInt ':-> C a ':-> C ('Vector i a) ':-> C ('Vector i a))
+    Get   :: CExpr (C ('TVector i a) ':-> CInt ':-> C a)
+    Put   :: CExpr (CInt ':-> C a ':-> C ('TVector i a) ':-> C ('TVector i a))
 
-data CCore = forall e. CCore { unCore :: Core CExpr e }
+type CCore = Core CExpr
+
+data ECore where
+    EEnd  :: ECore
+    EPush :: ECore
+    EEval :: forall (t :: Type TyPrim). Sing t -> CCore t -> ECore
--- src/Language/SessionTypes/RwSession.hs
+++ src/Language/SessionTypes/RwSession.hs
@@ -1,13 +1,100 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ViewPatterns #-}
 module Language.SessionTypes.RwSession
 ( CCore
-, Lbl (..)
 , ParSession
+, RoleSpec (..)
+, embed
+, step
 ) where
+
+import qualified Data.Set as Set
+import Data.Singletons
 
 import Language.Internal.Id
 import Language.Poly.C
 import Language.SessionTypes
 
-data Lbl = Lbl { lblname :: String, lblpre :: CCore, lblpost :: CCore }
+-- []          m -> w : A { [] } . [
+-- f : A -> B
 
-type ParSession = GT Id CType Lbl
+type ParSession = GT Id (Type PrimTy) ECore
+
+class Monad m => GenMonad m v where
+  fresh :: String -> m v
+
+embed :: forall a b m. (SingI a, SingI b, GenMonad m Role)
+      => CCore (a ':-> b) -> m ParSession
+embed f =
+    do m <- fresh "m"
+       w <- fresh "w"
+       return $
+         Msg m (Set.singleton w) (fromSing t1) (EEval (t1 `STArr` t2) f)
+         ... Msg w (Set.singleton m) (fromSing t2) EEnd
+         ... GEnd
+  where
+    t1 :: Sing a
+    t2 :: Sing b
+    t1 = sing
+    t2 = sing
+
+data RoleSpec = RK Role | RProd Role Role
+
+-- Substitute one rolename for another rolename in a global type.
+-- Substitute a role by a pair of roles
+subst :: RoleSpec -> Role -> GT v t a  -> GT v t a
+subst _r1 _r2 _g = undefined
+
+step :: GenMonad m Role => ParSession -> m ParSession
+
+-- Rule: end-protocol
+step (Comm msg@(msgAnn -> EEval _ Id) GEnd) =
+    return $ msg { msgAnn = EEnd } ... GEnd
+step (Comm msg@(msgAnn -> EPush ) GEnd) =
+    return $ msg { msgAnn = EEnd } ... GEnd
+
+-- Rule: short-circuit
+-- m -> ws : A @ { id } . G
+--    === G [ m / ws ]     <=== G /= end
+step   (Comm (Msg  rf  rt _pl (EEval (_t1 `STArr` _t1') Id)) cont) =
+    return $ Set.foldl' (\f x -> f . subst (RK rf) x) id rt cont
+
+-- m -> ws : A @ { f . g } . G
+--    === m -> w : A @ { f } . w -> ws : B @ { g } . G
+--
+step (Comm (Msg rf rt _pl (EEval (STArr _t1 _t3) (f `Comp` g))) cont) =
+  do w1 <- fresh "w"
+     return $
+       Msg rf (Set.singleton w1) (fromSing (dom fty)) (EEval fty f)
+       ... Msg w1 rt (fromSing (cod fty)) (EEval gty g)
+       ... cont
+  where
+    fty = typeOf f
+    gty = typeOf g
+
+-- m -> {w1, ..., wn} : A @ {f /\ g}. G
+--     ===   m -> {w11, ..., wn1} : A @ { f }
+--         . m -> {w12, ..., wn2} : A @ { g }
+--         . G [ w11 x w12 / w1, ..., wn1 x wn2 / wn ]
+--
+--  where
+--    (r -> r' : A x B @ { f }) [r1 x r2 / r] ~~~> r1 -> r' : A . r2 -> r' : B @ --    { f }
+--     f (x,y) = ...
+--
+--  FIXME: annotations not on every message, but on particular points:
+--    r -> m : A . G puts a value of type 'A' in the context. The function in
+--
+--  In Haskell this corresponds to using a Maybe ann as annotation for Msg.
+--
+-- With choice, if there is a context T, a choice:
+--
+-- r -> m : { l1 : G1, ... ln : Gn} means that, at the beginning of Gi there
+-- will be a value in a context of sum type  T_1 + ... + T_i-1 + T + T_i+1 + ...  -- + T_n
+
+step x = return x
